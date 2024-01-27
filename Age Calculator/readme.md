1eb0218b717fbf9d631f00e694a728aaa002ffb8
T1-Praveen <157484020+T1-Praveen@users.noreply.github.com>
2024-01-27 21:11:09 +0530

    Add files via upload

---
+++ Age Calculator/readme.md
@@ -0,0 +1,10 @@
+you can improve my code.
+there is a little feature you can improve.
+
+this improvement is when you age calculate then age is display but this is not automaticaly close
+
+then you can improve this code and send me.
+
+i can learn javascript and you can help me.
+
+practice! practice! practice!!!
---
+++ Age Calculator/script.js
@@ -0,0 +1,21 @@
+// console.log("hello world");
+
+let currDate = document.querySelector(".currDate");
+let dateOfBirth = document.querySelector("#dob");
+const CalcAge = document.getElementById("CalcAge");
+const displayAge = document.getElementById("displayAge");
+const Age = document.getElementById("age");
+var today = new Date();
+
+currDate.innerText = `Today's Date is: ${today.toLocaleDateString('en-US')}`;
+
+CalcAge.addEventListener("click",()=>{
+    var birthDate = new Date(dateOfBirth.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if(m < 0 || (m === 0 && today.getDate() < birthDate.getDate())){
+        age = age - 1;
+    }
+    displayAge.style.visibility = "visible";
+    Age.innerText = `You are ${age} years old.`
+});
